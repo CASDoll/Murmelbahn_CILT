95116d5a1fe9a1d272e7b297f3ca912d72100cca
SirMyrkus <114597000+SirMyrkus@users.noreply.github.com>
2024-01-31 08:40:14 +0100

    -

---
+++ Assets/MyGame/Marggus/script/ObjectSnapper.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSnapper : MonoBehaviour
+{
+    private const float snapAngle = 45f;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SnapObjectToNearestAngle();
+        }
+    }
+
+    private void SnapObjectToNearestAngle()
+    {
+        Vector3 currentRotation = transform.rotation.eulerAngles;
+        float nearestAngle = Mathf.Round(currentRotation.y / snapAngle) * snapAngle;
+
+        transform.rotation = Quaternion.Euler(currentRotation.x, nearestAngle, currentRotation.z);
+    }
+}
