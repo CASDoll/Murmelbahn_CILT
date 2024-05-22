321efea4313e6a9d964f6f2173e423bc7da45928
TamasGalambos <tamas.galambos05@htl-salzburg.ac.at>
2024-05-22 09:25:59 +0200

    AdditveSceneLoading added

---
+++ Assets/MyGame/Scripts/AdditveSceneLoading.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class AdditveSceneLoading : MonoBehaviour
+{
+    void Start()
+    {
+        SceneManager.LoadScene("ConstanzeScene", LoadSceneMode.Additive);
+        SceneManager.LoadScene("NiclasScene", LoadSceneMode.Additive);
+        SceneManager.LoadScene("TamasScene", LoadSceneMode.Additive);
+        SceneManager.LoadScene("snappen", LoadSceneMode.Additive);
+        SceneManager.LoadScene("TobiasScene", LoadSceneMode.Additive);
+        SceneManager.LoadScene("TestIna", LoadSceneMode.Additive);
+    }
+}
