1c6b7eead276bea7fcffddd67de79e836a3971a5
Lukas427 <lukas.holleis05@htl-salzburg.ac.at>
2024-05-29 10:19:16 +0200

    Snapper Reworked, bugfix required

--- Assets/MyGame/Marggus/script/ObjectSnapper.cs
+++ Assets/MyGame/Marggus/script/ObjectSnapper.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,11 +7,15 @@ public class ObjectSnapper : MonoBehaviour
 {
     private const float snapAngle = 45f;
 
+    public Transform center;
+    public Transform[] localExits;
+
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            SnapObjectToNearestAngle();
+            //SnapObjectToNearestAngle();
+            SnapToExit();
         }
     }
 
@@ -19,5 +24,63 @@ public class ObjectSnapper : MonoBehaviour
         Vector3 currentRotation = transform.rotation.eulerAngles;
         float nearestAngle = Mathf.Round(currentRotation.y / snapAngle) * snapAngle;
         transform.rotation = Quaternion.Euler(currentRotation.x, nearestAngle, currentRotation.z);
+    }
+
+    public void SnapToExit()
+    {
+        //find the nearest exit to the center
+        GameObject[] exitsTemp = GameObject.FindGameObjectsWithTag("Exit");
+        List<GameObject> allExits = new List<GameObject>();
+        for (int i = 0; i < exitsTemp.Length; i++)
+        {
+            if (CheckExits(exitsTemp[i]))
+            {
+                allExits.Add(exitsTemp[i]);
+            }
+        }
+
+        Transform nearestExit = null;
+        float nearestExitDist = -1;
+        foreach (GameObject exit in allExits)
+        {
+            if (nearestExitDist > Vector3.Distance(center.position, exit.transform.position))
+            {
+                nearestExit = exit.transform;
+                nearestExitDist = Vector3.Distance(center.position, exit.transform.position);
+            }
+        }
+
+        //find which exit is closest to the nearest exit
+        Transform nearestLocalExit = null;
+        nearestExitDist = -1;
+        foreach (Transform localExit in localExits)
+        {
+            if (nearestLocalExit != null)
+            {
+                if (nearestExitDist > Vector3.Distance(localExit.position, nearestExit.position))
+                {
+                    nearestLocalExit = localExit;
+                }
+            }
+            else
+            {
+                nearestLocalExit = localExit;
+            }
+        }
+
+        //move the object accordingly
+        transform.position = transform.position + (nearestExit.position - nearestLocalExit.position);
+    }
+
+    private bool CheckExits(GameObject exitToCheck)
+    {
+        for (int i = 0; i < localExits.Length; i++)
+        {
+            if (exitToCheck == localExits[i])
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
