f4703c30d69633c2031e53fe92e5856e32cd5666
Ina Ebner <ina-ebner@aon.at>
2024-06-05 10:38:07 +0200

    LoadImages

--- Assets/MyGame/Ina/Scripts/LoadImage.cs
+++ Assets/MyGame/Ina/Scripts/LoadImage.cs
@@ -5,16 +5,23 @@ using UnityEngine.UI;
 
 public class LoadImage : MonoBehaviour
 {
-    public Image Image01;
-    public Image Image02;
-    public Image Image03;
-    public Image Image04;
+    public Image image01;
+    public Image image02;
+    public Image image03;
+    public Image image04;
 
     public Sprite[] Sprites;
+
+    public GameObject module01;
+    public GameObject module02;
+    public GameObject module03;
+    public GameObject module04;
+
+    //public bool moduleSelected = module01.image02;
     // Start is called before the first frame update
     void Start()
     {
-        
+        LoadSprites();
     }
 
     // Update is called once per frame
@@ -26,6 +33,24 @@ public class LoadImage : MonoBehaviour
     //random Sprite aus Sprites Array wird Image zugewiesen
     public void LoadSprites()
     {
-        Image01.sprite = Sprites[Random.Range(0, Sprites.Length)];
+        image01.sprite = Sprites[Random.Range(0, Sprites.Length)];
+        image02.sprite = Sprites[Random.Range(0, Sprites.Length)];
+        image03.sprite = Sprites[Random.Range(0, Sprites.Length)];
+        image04.sprite = Sprites[Random.Range(0, Sprites.Length)];
+    }
+
+    // ein Modul wird aus der Liste ausgewählt
+    public void SelectModule()
+    {
+
+    }
+
+    // die Module werden neu geladen, wenn davor eines ausgewählt wurde
+    public void ResetImages()
+    {
+        if (moduleSelected == true)
+        {
+            LoadSprites();
+        }
     }
 }
