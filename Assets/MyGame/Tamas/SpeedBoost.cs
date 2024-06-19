088f175c43d9af30076bbc43f6968177897146d5
TamasGalambos <tamas.galambos05@htl-salzburg.ac.at>
2024-06-19 08:54:28 +0200

    SpeedBoost fixed

--- Assets/MyGame/Tamas/SpeedBoost.cs
+++ Assets/MyGame/Tamas/SpeedBoost.cs
@@ -8,23 +8,13 @@ public class SpeedBoost : MonoBehaviour
     Vector3 normalizedVelocity;
     public int speedForce;
 
-    void Awake()
-    {
-        
-    }
-
-    void Start()
-    {
-
-    }
-
     private void Update()
     {
         normalizedVelocity = gM.sceneBall.GetComponent<Rigidbody>().velocity.normalized;
         //Debug.Log(normalizedVelocity);
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerStay(Collider other)
     {
         gM.sceneBall.GetComponent<Rigidbody>().AddForce(normalizedVelocity * Time.deltaTime * speedForce);
         Debug.Log("BOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOST");
